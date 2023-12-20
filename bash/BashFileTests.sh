49b436da618a882e95a27afb4550d4bff644e2bb
vgartg <gopik539@mail.ru>
2023-12-21 00:09:26 +0300

    Added tests for BashFile

---
+++ bash/BashFileTests.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+. /bash/BashFile.sh
+
+if [ ! $[$(Prod 1 1)==1] ]
+then exit 99;
+fi
+
+if [ ! $[$(Prod 2 2)==4] ]
+then exit 99; 
+fi
+
+if [ ! $[$(Prod 0 1234)!=1234] ]
+then exit 99;
+fi
+
+echo "Tests compeleted!"
